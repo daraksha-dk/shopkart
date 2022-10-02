22d7f0cbf3bf03530d85dafb1178bf3182e29fa0
daraksha <daraksha.atg@gmail.com>
2022-10-02 15:08:15 +0530

    initial setup

---
+++ src/components/directory/DirectoryItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./DirectoryItem.scss";
+import { withRouter } from "react-router-dom";
+const DirectoryItem = ({ title, img, size, history, path }) => {
+  return (
+    <div
+      className={`${size} directory-item`}
+      onClick={() => history.push(path)}
+    >
+      <div
+        className="background-img"
+        style={{ backgroundImage: `url(${img})` }}
+      />
+      <div className="content">
+        <h1 className="title">{title}</h1>
+        <span className="subtitle">Shop Now</span>
+      </div>
+    </div>
+  );
+};
+
+export default withRouter(DirectoryItem);
